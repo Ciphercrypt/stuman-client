d98e86c5cbe2ba3b56e16ecc3e388b4c8698ce7e
Avishkar Mohite <avimohite2019@gmail.com>
2022-01-28 13:32:35 +0530

    htw
    q

---
+++ build/precache-manifest.ceb12d7251335837807367ed4ad8cb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecac0ef1c92840aee3cfdcdfeb83a03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f541fdc8f2ae6d850a67",
+    "url": "/static/css/main.dc9534fa.chunk.css"
+  },
+  {
+    "revision": "7b4787f4d2fcfcc7b376",
+    "url": "/static/js/2.cd52892b.chunk.js"
+  },
+  {
+    "revision": "897b0102677582fcaa808171bd1ec14b",
+    "url": "/static/js/2.cd52892b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f541fdc8f2ae6d850a67",
+    "url": "/static/js/main.477c9370.chunk.js"
+  },
+  {
+    "revision": "35dc8619f58f92dc1888",
+    "url": "/static/js/runtime-main.b83e4067.js"
+  }
+]);
